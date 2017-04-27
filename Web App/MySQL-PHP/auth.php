99abbe620bef1d4325fdc769b2f590ca514acffc
Raymond Flanagan <tikthra@gmail.com>
2017-04-27 21:23:49 +0100

    Upgrades to accounts PHP - change pass/forgot handling added + SQL injection prevention added to the parsing of the data sent from the app

--- Web App/MySQL-PHP/auth.php
+++ Web App/MySQL-PHP/auth.php
@@ -2,16 +2,16 @@
 header("Access-Control-Allow-Origin: *");
 
 //Connect & Select Database
-$con = mysqli_connect("eu-cdbr-azure-north-e.cloudapp.net","68c0ba5cbf74ef7","68c0ba5cbf74ef7","acsm_7e1519900d8edad") or die("could not connect server");
+$con = mysqli_connect("localhost","root","","usersECOWATER") or die("could not connect server");
 
 //Create New Account
 if(isset($_POST['signup']))
 {
 	// Avoiding SQL Injection & removing spl chars
-	$fullname=$_POST['fullname']);
-	$email=$_POST['email'];
-	$heightincm=$_POST['heightincm'];
-	$password=$_POST['password']);
+	$fullname=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['fullname'])));
+	$email=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['email'])));
+	$heightincm=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['heightincm'])));
+	$password=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['password'])));
 
 	$heightInCM_INT=(int)$heightincm;
 	
@@ -41,9 +41,9 @@ if(isset($_POST['signup']))
 //Login
 else if(isset($_POST['login']))
 {
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-	$login=mysqli_query($con,"SELECT * FROM `phonegap_login` WHERE `email`='$email' AND `password`='$password'");
+	$email=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['email'])));
+	$password=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['password'])));
+	$login=mysqli_num_rows(mysqli_query($con,"SELECT * FROM `phonegap_login` WHERE `email`='$email' AND `password`='$password'"));
 	if($login!=0)
 	{
 		echo "success";
@@ -53,6 +53,42 @@ else if(isset($_POST['login']))
 		echo "error";
 	}
 }
+
+//Change Password
+else if(isset($_POST['change_password']))
+{
+	$email=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['email'])));
+	$old_password=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['old_password'])));
+	$new_password=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['new_password'])));
+	$check=mysqli_num_rows(mysqli_query($con,"SELECT * FROM `phonegap_login` WHERE `email`='$email' AND `password`='$old_password'"));
+	if($check!=0)
+	{
+		mysqli_query($con,"UPDATE `phonegap_login` SET `password`='$new_password' WHERE `email`='$email'");
+		echo "success";
+	}
+	else
+	{
+		echo "incorrect";
+	}
+}
+
+// Forget Password
+else if(isset($_POST['forget_password']))
+{
+	$email=mysqli_real_escape_string($con,htmlspecialchars(trim($_POST['email'])));
+	$q=mysqli_query($con,"SELECT * FROM `phonegap_login` WHERE `email`='$email'");
+	$check=mysqli_num_rows($q);
+	if($check!=0)
+	{
+		echo "success";
+		$data=mysqli_fetch_array($q);
+		$string="Hey,".$data['fullname'].", Your password is".$data['password'];
+		mail($email, "Your Password", $string);
+	}
+	else
+	{
+		echo "invalid";
+	}
 }
 echo mysqli_error($con);
 ?>
